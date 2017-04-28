94be733efaf9cf8d9ec1b8d541e29600d15c6c4f
Bruno Filho <brgccf@cin.ufpe.br>
2017-04-28 13:47:50 -0300

    Atualizando projeto para Rails 5.0.0

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_GestaoResiduosApp_session'
+Rails.application.config.session_store :cookie_store, key: '_gestao_residuos_app_session'
