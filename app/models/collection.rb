66216792ab87eac95a894c8f35caf1240c436b2a
Lucas Alves Rufino <lar@cin.ufpe.br>
2017-06-06 21:47:05 +0000

    Correção de conflitos para adição de modificações da views

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -4,4 +4,48 @@ class Collection < ApplicationRecord
   has_one :notification, dependent: :destroy
   
   validates :max_value, presence: true
+  
+  attr_accessor :mean, :miss_days, :miss_weight, :solido_organico, :solido_inorganico, :liquido_organico, :liquido_inorganico, :liquido_inflamavel, :outros
+  
+  def generate_prediction
+    collection = Collection.last
+    weight = 0.0
+    Register.all.each do |register|
+      weight += register.weight
+    end
+    time = Date.today - collection.created_at.to_date
+    mean = weight/time
+    miss_weight = (collection.max_value - weight)
+    miss_days = miss_weight/mean
+    miss_days = miss_days.ceil
+    @mean=mean
+    @miss_days=miss_days
+    @miss_weight=miss_weight
+  end
+  
+  def type_residue
+    @solido_organico = 0.0
+    @solido_inorganico = 0.0
+    @liquido_organico = 0.0
+    @liquido_inorganico = 0.0
+    @liquido_inflamavel = 0.0
+    @outros  = 0.0
+    Register.all.each do |register|
+      residue = Residue.find_by(id: register.residue_id)
+      case residue.kind
+      when "Sólido Orgânico"
+        @solido_organico += register.weight
+      when "Sólido Inorgânico"
+        @solido_inorganico += register.weight
+      when "Líquido Orgânico"
+        @liquido_organico += register.weight
+      when "Líquido Inorgânico"
+        @liquido_inorganico += register.weight
+      when "Líquido Inflamável"
+        @liquido_inflamavel += register.weight
+      when "Outros"
+        @outros += register.weight
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,13 @@ Rails.application.routes.draw do
   resources :departments
   resources :registers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'application#index'
+  
+  get '/main_fac', to: 'application#main_fac'
+  get '/main_adm', to: 'application#main_adm'
+  get '/genarate_report', to: 'application#genarate_report'
+  get '/statistic', to: 'application#statistic'
+  get '/request', to: 'application#request'
+  get '/account', to: 'application#account'
 end
