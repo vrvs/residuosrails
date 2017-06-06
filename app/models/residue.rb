efa27e757d2e62a56b72cf17c016c072a2041094
Lucas Alves Rufino <lar@cin.ufpe.br>
2017-06-06 19:17:19 +0000

    Códigos de registros refatorados e cenarios de controlador implementados

--- app/models/residue.rb
+++ app/models/residue.rb
@@ -6,20 +6,4 @@ class Residue < ApplicationRecord
   def total_res
     self.registers.sum(:weight)
   end
-  
-  def compare_report_att(res, filter)
-    if filter.kind and self.kind != res.kind then
-      return false
-    elsif filter.onu and self.onu != res.onu then
-      return false
-    elsif filter.code and self.code != res.code then
-      return false
-    elsif filter.blend and self.blend != res.blend then
-      return false
-    end
-    true
-  end
-  
 end
-
-
--- features/step_definitions/geracao_de_relatorios_steps.rb
+++ features/step_definitions/geracao_de_relatorios_steps.rb
@@ -1,21 +1,21 @@
 Given(/^o sistema possui o departamento de "([^"]*)" cadastrado$/) do |dep_name|
-  dep = createDepartment({department: {name: dep_name}})
+  dep = create_department({department: {name: dep_name}})
   expect(dep).to_not be nil
 end
 
 Given(/^o sistema possui o laboratório de "([^"]*)" cadastrado no departamento de "([^"]*)"$/) do |lab_name, dep_name|
   dep = Department.find_by(name: dep_name)
   expect(dep).to_not be nil
-  lab = createLaboratory({laboratory: {name: lab_name, department_id: dep.id}})
+  lab = create_laboratory({laboratory: {name: lab_name, department_id: dep.id}})
  end 
 
 Given(/^o sistema possui "([^"]*)"kg de resíduos cadastrados no laboratório de "([^"]*)"$/) do |res_weight, lab_name|
   lab = Laboratory.find_by(name: lab_name)
   expect(lab).to_not be nil
-  res = createResidue({residue:{name: "Acido", laboratory_id: lab.id}})
+  res = create_residue({residue:{name: "Acido", laboratory_id: lab.id}})
   expect(res).to_not be nil
-  createRegister({register: {weight: res_weight.to_f(), residue_id: res.id}})
-  reg = modifyDateLastRegister(res.id, "23/02/2017")
+  create_register({register: {weight: res_weight.to_f(), residue_id: res.id}})
+  reg = modify_date_last_register(res.id, "23/02/2017")
   expect(reg).to_not be nil
   expect(res.total_res).to eq(res_weight.to_f())
   end
@@ -44,15 +44,15 @@ Given(/^o sistema possui "([^"]*)" kg de resíduos cadastrados entre entre as da
 	if(res != nil) then
 	  reg = {register: {weight: res_weight.to_f(), residue_id: res.id}}
   else
-    res = createResidue({residue: {name:"Acido", laboratory_id: lab.id}})
-    modifyDateLastRegister(res_id,data_begin.to_date)
+    res = create_residue({residue: {name:"Acido", laboratory_id: lab.id}})
+    modify_date_last_register(res_id,data_begin.to_date)
     put_date(res,data_begin.to_date)
     reg = {register: {weight: res_weight.to_f(), residue_id: res.id}}
   end
 	expect(res).to_not be nil
 	post '/update_weight', reg
 
-	modifyDateLastRegister(res_id,data_begin.to_date)
+	modify_date_last_register(res_id,data_begin.to_date)
 	res = Residue.where(laboratory_id: lab.id)
   expect(res).to_not be nil
 	sum_registers(res,data_begin,data_final)
@@ -62,13 +62,13 @@ Given(/^o sistema possui "([^"]*)" kg de resíduos cadastrados entre entre as da
 end
 
 Given(/^o sistema possui o departamento de "([^"]*)" cadastrado com o resíduo "([^"]*)" com quantidade total de "([^"]*)"Kg$/) do |dep_name, res_name, res_total|
-  dep = createDepartment({department: {name: dep_name}})
+  dep = create_department({department: {name: dep_name}})
   expect(dep).to_not be nil
-  lab = createLaboratory({laboratory: {name: "lab_base: " + dep_name, department_id: dep.id}})
+  lab = create_laboratory({laboratory: {name: "lab_base: " + dep_name, department_id: dep.id}})
   expect(lab).to_not be nil
-  res = createResidue({residue: {name: res_name, laboratory_id: lab.id}})
+  res = create_residue({residue: {name: res_name, laboratory_id: lab.id}})
   expect(res).to_not be nil
-  reg = createRegister({register: {weight: res_total.to_f(), residue_id: res.id}})
+  reg = create_register({register: {weight: res_total.to_f(), residue_id: res.id}})
   expect(reg.weight).to eq(res_total.to_f())
 end
 
@@ -89,81 +89,72 @@ When(/^eu tento gerar um relatório dos resíduos dos departamentos de "([^"]*)"
   post '/reports', rep
 end
 
-Then(/^o sistema retorna o valor de "([^"]*)"Kg para o resíduo "([^"]*)"$/) do |arg1, arg2|
-  repc = Reportcell.where(res_name: arg2)
+Then(/^o sistema retorna o valor de "([^"]*)"Kg para o resíduo "([^"]*)"$/) do |res_weight, res_name|
+  repc = Reportcell.where(res_name: res_name)
   total = 0
   repc.each do |it|
     total += it.total
   end
-  expect(total).to eq(arg1.to_f())
+  expect(total).to eq(res_weight.to_f())
 end
 
 Given(/^o resíduo "([^"]*)" possui tipo como "([^"]*)", peso como "([^"]*)"Kg e código ONU como "([^"]*)" no laboratorio de "([^"]*)"$/) do |res_name, res_kind, res_weight, res_onu, lab_name|
   lab = Laboratory.find_by(name: lab_name)
   expect(lab).to_not be nil
-  res = {residue: {name: res_name, kind: res_kind, onu: res_onu, laboratory_id: lab.id}}
-  post '/residues', res
-  res = Residue.find_by(name: res_name, laboratory_id: lab.id)
+  res = create_residue({residue: {name: res_name, kind: res_kind, onu: res_onu, laboratory_id: lab.id}})
   expect(res).to_not be nil
-  reg = {register: {weight: res_weight.to_f(), residue_id: res.id, department_id: lab.department_id, laboratory_id: lab.id}}
-  post '/update_weight', reg
-  reg = res.registers.last
+  reg = create_register({register: {weight: res_weight.to_f(), residue_id: res.id}})
   expect(reg.weight).to eq(res_weight.to_f())
 end
 
-When(/^eu tento produzir um relatório dos resíduos do laboratório de "([^"]*)", com os filtros tipo e peso\.$/) do |arg1|
-  rep = {report: {generate_by: 2, begin_date: "01/01/2001".to_date, end_date: "29/12/2029".to_date, f_unit: false, f_state: false, f_kind: true, f_onu: false, f_blend: false, f_code: false, f_total: true}}
+When(/^eu tento produzir um relatório dos resíduos do laboratório de "([^"]*)", com os filtros tipo e peso\.$/) do |res|
+  rep = {report: {
+    generate_by: 2, 
+    begin_dt: "01/01/2001".to_date, 
+    end_dt: "29/12/2029".to_date, 
+    unit: false, 
+    state: false, 
+    kind: true, 
+    onu: false, 
+    blend: false, 
+    code: false, 
+    total: true,
+    list: [res]}
+  }
   post '/reports', rep
-  repc = {reportcell: {lab_name: arg1}}
-  post '/generate_reportcell', repc
 end
 
-Then(/^o sistema retorna as informações "([^"]*)" e "([^"]*)"Kg para o resíduo "([^"]*)"$/) do |arg1, arg2, arg3|
-  repc = Reportcell.where(res_name: arg3)
+Then(/^o sistema retorna as informações "([^"]*)" e "([^"]*)"Kg para o resíduo "([^"]*)"$/) do |kind, quant, res_name|
+  repc = Reportcell.where(res_name: res_name)
   total = 0
   repc.each do |it|
     total += it.total
   end
-  expect(total).to eq(arg2.to_f())
-  expect(repc[0].kind).to eq (arg1)
+  expect(total).to eq(quant.to_f())
+  expect(repc[0].kind).to eq (kind)
 end
 
-
-def sum_registers(res,data_begin,data_final)
-     residues_total_in_data = 0
-     res.each do |it|
-	    rList = it.registers.where(created_at: [data_begin.to_date..data_final.to_date])
-      expect(rList).to_not be nil
-      r = rList.order(:created_at)
-     
-      residues_total_in_data = residues_total_in_data + (rList.last.weight - rList.first.weight)
-    end
-   residues_total_in_data
-end
-  
-
-
-def createDepartment(dep)
+def create_department(dep)
   post '/departments', dep
   Department.find_by(name: dep[:department][:name])
 end
 
-def createLaboratory(lab)
+def create_laboratory(lab)
   post '/laboratories', lab
   Laboratory.find_by(name: lab[:laboratory][:name], department_id: lab[:laboratory][:department_id])
 end
 
-def createResidue(res)
+def create_residue(res)
   post '/residues', res
   Residue.find_by(name: res[:residue][:name], laboratory_id: res[:residue][:laboratory_id])
 end
 
-def createRegister(reg)
+def create_register(reg)
   post '/registers', reg
   Residue.find(reg[:register][:residue_id]).registers.last
 end
 
-def modifyDateLastRegister(res_id, date)
+def modify_date_last_register(res_id, date)
   reg = Residue.find(res_id).registers.last
   reg.created_at = date.to_date
   reg.save
